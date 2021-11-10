714d2bd00ea29b9911d15e15d44e37d0d5546714
3ba2ii <aghonem2011@gmail.com>
2021-11-10 14:49:19 +0200

    fix: 🐛 fixed a typo in github actions

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-echo What should the version be?
-read VERSION
-
-echo Starte Build Docker Image & Pushing to Docker Hub
-
-docker build -t 3ba2ii/paaws:$VERSION .
-docker push 3ba2ii/paaws:$VERSION
-
 echo 🥳 Docker Built Successfully!
 
 echo Starts pulling the image & deploying
-ssh root@165.22.16.231 "docker pull 3ba2ii/paaws:$VERSION && docker tag 3ba2ii/paaws:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
+ssh root@165.22.16.231 "docker pull 3ba2ii/paaws:latest && docker tag 3ba2ii/paaws:latest dokku/api:latest && dokku deploy api latest"
 
 echo 🥳 Deployed Successfully!

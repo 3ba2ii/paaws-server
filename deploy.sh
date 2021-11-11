4ae5fa854c0bc384f83af6cfb04a19d356bd87a0
3ba2ii <aghonem2011@gmail.com>
2021-11-11 16:13:10 +0200

    ci: 🎡 updated the script responsible for deploying

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 echo "🥳 Docker Built Successfully!"
 
 echo "Starts pulling the image & deploying"
-ssh root@165.22.16.231 "docker pull 3ba2ii/paaws:latest && docker tag 3ba2ii/paaws:latest dokku/api:latest && dokku deploy api latest"
+ssh root@46.101.166.220 "docker pull 3ba2ii/paaws:latest && docker tag 3ba2ii/paaws:latest dokku/api:latest && dokku deploy api latest"
 
 echo "🥳 Deployed Successfully!"

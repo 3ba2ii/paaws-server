b0f50d4fa1a4c7099047fb454f217f9bfc5cfab7
3ba2ii <aghonem2011@gmail.com>
2022-06-20 19:25:55 +0200

    feat: 🎸 added methods to update and check user's settings

--- src/repos/SettingRepo.repo.ts
+++ src/repos/SettingRepo.repo.ts
@@ -1,8 +1,15 @@
-import { UserSetting } from './../entity/Settings/UserSettings';
-import { Setting } from '../entity/Settings/Settings';
+import { RegularResponse } from 'src/types/response.types';
 import { Service } from 'typedi';
 import { EntityRepository, Repository } from 'typeorm';
+import { Setting } from '../entity/Settings/Settings';
 import { User } from '../entity/UserEntities/User';
+import { UserSetting } from './../entity/Settings/UserSettings';
+import {
+  CREATE_ALREADY_EXISTS_ERROR,
+  CREATE_NOT_FOUND_ERROR,
+  INTERNAL_SERVER_ERROR,
+} from './../errors';
+import { UpdateUserSettingsInput } from './../types/input.types';
 
 @Service()
 @EntityRepository(Setting)
@@ -45,5 +52,98 @@ export class SettingRepo extends Repository<Setting> {
       language: 'EN',
       accountURL: await this.createUniqueAccountURL(user, 0),
     }); //
+  }
+
+  private trimAndLower(str: string) {
+    return str.trim().toLocaleLowerCase().replace(' ', '');
+  }
+
+  public async isValidAccountURL(accountURL: string): Promise<boolean> {
+    const trimmedURL = this.trimAndLower(accountURL);
+
+    const us = await UserSetting.findOne({ accountURL: trimmedURL });
+
+    return us ? false : true;
+  }
+
+  public async updateUserSettingsURL(
+    userId: number,
+    accountURL: string
+  ): Promise<RegularResponse> {
+    const userSettings = await UserSetting.findOne({ where: { userId } });
+    if (!userSettings) {
+      return {
+        success: false,
+        errors: [
+          CREATE_NOT_FOUND_ERROR('user-settings', 'No user settings found'),
+        ],
+      };
+    }
+    //check if its the same account URL
+    if (userSettings.accountURL === accountURL) {
+      return {
+        success: false,
+        errors: [
+          {
+            code: 401,
+            field: 'accountURL',
+            message: 'The account url is the same as before',
+          },
+        ],
+      };
+    }
+
+    //check if the account URL is a valid one (Not duplicated)
+    const isValid = await this.isValidAccountURL(accountURL);
+    if (!isValid) {
+      return {
+        success: false,
+        errors: [
+          CREATE_ALREADY_EXISTS_ERROR(
+            'accountURL',
+            'This account URL is already in use.'
+          ),
+        ],
+      };
+    }
+    userSettings.accountURL = this.trimAndLower(accountURL);
+
+    try {
+      await userSettings.save();
+      return { success: true };
+    } catch (e) {
+      return { success: false, errors: [e, INTERNAL_SERVER_ERROR] };
+    }
+  }
+
+  public async updateUserSettings(
+    userId: number,
+    updateSettingsInput: UpdateUserSettingsInput
+  ): Promise<RegularResponse> {
+    const userSettings = await UserSetting.findOne({ where: { userId } });
+    if (!userSettings) {
+      return {
+        success: false,
+        errors: [
+          CREATE_NOT_FOUND_ERROR('user-settings', 'No user settings found'),
+        ],
+      };
+    }
+    if (typeof updateSettingsInput.showEmail === 'boolean') {
+      userSettings.showEmail = updateSettingsInput.showEmail;
+    }
+    if (typeof updateSettingsInput.showPhoneNumber === 'boolean') {
+      userSettings.showPhone = updateSettingsInput.showPhoneNumber;
+    }
+    if (typeof updateSettingsInput.language === 'string') {
+      userSettings.language = updateSettingsInput.language;
+    }
+
+    try {
+      await userSettings.save();
+      return { success: true };
+    } catch (e) {
+      return { success: false, errors: [e, INTERNAL_SERVER_ERROR] };
+    }
   }
 }
